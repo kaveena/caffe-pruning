47012a47fb0e319be3ac0f068641e090bf777d92
kaveena <persandk@scss.tcd.ie>
2018-12-18 11:33:41 +0000

    add ddiff propagation for conv saliency layer

--- src/caffe/layers/conv_saliency_layer.cu
+++ src/caffe/layers/conv_saliency_layer.cu
@@ -29,12 +29,15 @@ void ConvolutionSaliencyLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& t
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
   const Dtype* weight = this->blobs_[0]->gpu_data();
   Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
+  Dtype* weights_sqr = this->weights_sqr_.mutable_gpu_data();
+  caffe_gpu_powx(this->blobs_[0]->count(), weight, (Dtype)2, weights_sqr);
   for (int i = 0; i < top.size(); ++i) {
     const Dtype* top_diff = top[i]->gpu_diff();
     const Dtype* top_ddiff = top[i]->gpu_ddiff();
     const Dtype* top_data = top[i]->gpu_data();
     const Dtype* bottom_data = bottom[i]->gpu_data();
     Dtype* bottom_diff = bottom[i]->mutable_gpu_diff();
+    Dtype* bottom_ddiff = bottom[i]->mutable_gpu_ddiff();
     // Bias gradient, if necessary.
     if (this->bias_term_ && this->param_propagate_down_[1]) {
       Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
@@ -53,6 +56,10 @@ void ConvolutionSaliencyLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& t
         if (propagate_down[i]) {
           this->backward_gpu_gemm(top_diff + n * this->top_dim_, weight,
               bottom_diff + n * this->bottom_dim_);
+        }
+        if (propagate_down[i]) {
+          this->backward_gpu_gemm(top_ddiff + n * this->top_dim_, weights_sqr,
+              bottom_ddiff + n * this->bottom_dim_);
         }
       }
     }
