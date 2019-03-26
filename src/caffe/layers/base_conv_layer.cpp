3261cbfe56ac0874dc433ca9268df06a0c75402d
Andrew Anderson <andrew.wja@gmail.com>
2019-03-26 20:48:51 +0000

    Fix quantization mask on exponent

--- src/caffe/layers/base_conv_layer.cpp
+++ src/caffe/layers/base_conv_layer.cpp
@@ -150,7 +150,7 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   if (this->quantize_term_) {
     int qrbits = this->layer_param_.convolution_quantize_param().quantization_range_bits();
     uint64_t qrbits_mask = (1 << qrbits) - 1;
-    qrbits_mask <<= (mantissa_length<Dtype>() + exponent_length<Dtype>() - 1) - qrbits;
+    qrbits_mask <<= mantissa_length<Dtype>();
     int qpbits = this->layer_param_.convolution_quantize_param().quantization_precision_bits();
     uint64_t qpbits_mask = (1 << qpbits) - 1;
     qpbits_mask <<= (mantissa_length<Dtype>() - qpbits);
